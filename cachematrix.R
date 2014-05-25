468a9e21d243c4f5b41ea6329dd6469658f6eae4
Ram <muthukrishnan.s@gmail.com>
2014-05-25 15:26:26 -0700

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to Cache the Inverse of a Matrix
 
-## Write a short comment describing this function
+## There are two functions
 
-makeCacheMatrix <- function(x = matrix()) {
+## 1. makeCacheMatrix <- function(x = matrix) 
+## Above function creates a special "matrix" object X that can cache its inverse
 
+## 2. cacheSolve <- function (y, ...)
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
+
+## Returns a list having function Set, Get, SetInv and GetInv
+makeCacheMatrix <- function(x = matrix)
+{
+    c = ncol(x)
+    r = nrow(x)
+    
+    xInv <- NULL
+    
+    set <- function(y = matrix)
+    {
+        x <<- y
+        xInv <- NULL
+    }
+    
+    get <- function() x = matrix(x, r, c)
+    setInv <- function(x)
+    {
+        xInv <<- x
+    }
+    getInv <- function() xInv
+    
+    list(Set = set, Get = get,
+         SetInv = setInv,
+         GetInv = getInv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Checks for Inverse matrix data in env, if not available then try to create it and print
+cacheSolve <- function (y, ...)
+{
+    zInv <- y$GetInv()
+    if(!is.null(zInv))
+    {
+        print("Got cached inverse data")
+        print(zInv)
+        return(zInv)
+    }
+    
+    z <- matrix(y$Get(), nrow(y$Get()), ncol(y$Get()))
+    zInv <- solve(z)
+    y$SetInv(zInv)
+    print(zInv)
 }
